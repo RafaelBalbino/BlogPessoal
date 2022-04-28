4e84375d3e5c5a22d1c844369f61b71c617a67a3
RafaelBalbino <rafaelballabi@hotmail.com>
2022-04-28 17:38:19 -0300

    Atualizado em 28.04.2022

---
+++ Projeto Blog Pessoal/Inserindo Dados no db_blogpessoal.sql
@@ -0,0 +1,12 @@
+USE db_blogpessoal;
+
+INSERT tb_postagem (titulo, texto)
+VALUES ("O Homem de Preto", "Esse homem caça uns aligenígena muito cabulosos");
+
+INSERT tb_postagem (titulo, texto)
+VALUES ("Charlie Brown", "O homem quando anda de skate não quer guerra com ninguém.");
+
+INSERT tb_postagem (titulo, texto)
+VALUES ("O Hokage", "Esse Rafaelitos é hokage mesmo, hein?");
+
+SELECT * FROM tb_postagem;
