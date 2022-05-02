9499c118f08feae23619224ff123da6b3fc13a0e
RafaelBalbino <rafaelballabi@hotmail.com>
2022-05-02 13:14:04 -0300

    Atualizado em 02.05.2022

--- Projeto Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ Projeto Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -17,41 +17,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.generation.blogpessoal.model.Postagem;
-import com.generation.blogpessoal.repository.BlogPessoalRepository;
+import com.generation.blogpessoal.model.Tema;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 @RestController
-@RequestMapping ("/postagem")
+@RequestMapping ("/tema")
 @CrossOrigin(origins = "*")
-public class BlogPessoalController {
+// Não precisa do 'origins', só o asterisco basta
+public class TemaController {
 	
 	@Autowired
-	private BlogPessoalRepository repository;
+	private TemaRepository repository;
 	
 	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll() {
+	public ResponseEntity<List<Tema>> getAll() {
 		return ResponseEntity.ok(repository.findAll());
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity <Postagem> getById(@PathVariable Long id) {
+	public ResponseEntity <Tema> getById(@PathVariable Long id) {
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
 				                      .orElse(ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping("/titulo/{titulo}")
-	public ResponseEntity <List <Postagem>> getByTitulo(@PathVariable String titulo) {
-		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
-	}
-	
 	@PostMapping
-	public ResponseEntity <Postagem> post(@Valid @RequestBody Postagem postagem) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	public ResponseEntity <Tema> post(@Valid @RequestBody Tema tema) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
 	}
 	
 	@PutMapping
-	public ResponseEntity <Postagem> put(@Valid @RequestBody Postagem postagem) {
-		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	public ResponseEntity <Tema> put(@Valid @RequestBody Tema tema) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
 	}
 	
 	@DeleteMapping("/{id}")
--- Projeto Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ Projeto Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table (name = "tb_postagem")
@@ -31,6 +34,11 @@ public class Postagem {
 	@Temporal (TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
 
+	@NotNull //Aí é necessário cadastrar uma tema quando cadastrar uma postagem
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	public Long getId() {
 		return id;
 	}
@@ -61,5 +69,13 @@ public class Postagem {
 
 	public void setData(Date data) {
 		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 }
