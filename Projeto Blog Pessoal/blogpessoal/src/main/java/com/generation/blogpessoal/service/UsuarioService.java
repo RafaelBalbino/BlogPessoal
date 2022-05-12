e959ef303e52da2ed5ad54c98df6a6f708ea713b
Rafael B. Balbino <49595519+RafaelBalbino@users.noreply.github.com>
2022-05-12 10:11:34 -0300

    Atualizado em 12.05.2022

--- Projeto Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ Projeto Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -38,7 +38,7 @@ public class UsuarioService {
 		return Optional.of(repository.save(usuario));
 	}
 	
-	public Optional<Usuario>alterarUsuario(Usuario usuario){
+	public Optional<Usuario>atualizarUsuario(Usuario usuario){
 		repository.findById(usuario.getId()).isPresent();
 		usuario.setSenha(criptografarSenha(usuario.getSenha()));
 		return Optional.of(repository.save(usuario));
